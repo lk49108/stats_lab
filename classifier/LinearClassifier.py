45138bbcb6e8971c8293e7cfe8efa97b66fe83a0
muellchef <muellchef@gmail.com>
2020-04-16 16:08:31 +0200

    Changed the Feature Extractor as well as Linear Classifier Class and started working on regression

--- classifier/LinearClassifier.py
+++ classifier/LinearClassifier.py
@@ -1,58 +1,80 @@
 from sklearn import linear_model
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedShuffleSplit
 from sklearn.preprocessing import scale
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import balanced_accuracy_score, plot_confusion_matrix
+import matplotlib.pyplot as plt
 import os
+from collections import Counter
 
 class LinearClassifier:
     feature_block = pd.DataFrame()
     model = linear_model
 
 
-    def __init__(self, features, model_type='logistic'):
+    def __init__(self, features, model_type='logistic', penalty = 'l2'):
 
+        if features.selection_type == 'relevant':
+            if model_type == 'logistic':
+                self.model = linear_model.LogisticRegression(penalty='none', max_iter=1000)
+        else:
+            if model_type == 'logistic':
+                self.model = linear_model.LogisticRegression(penalty='l2', solver = 'saga', max_iter=1000)
+
+        #treat_count = features.iloc[:,-1].value_counts()
+        #pd.set_option('display.max_rows', features.shape[0] + 1)
+        #print(features.sort_values(by = ['id']))
+        #if len(set(treat_count)) > 1:
+        #    features = features.iloc[:4*min(treat_count)]
         self.feature_block = features
-
-        if model_type == 'logistic':
-            self.model = linear_model.LogisticRegression(penalty='none')
-
-
-    def validateClassifier(self,fit, X_test, y_test):
-        y_out = fit.predict(X_test)
-        out_df = pd.DataFrame({'real_y':y_test, 'predicted_y':y_out})
-
-        acc_score = self.accuracy(out_df)
-        print(out_df)
-        print("The accuracy score is: ", acc_score)
-        #self.getvalidationset()
-
+        #pd.set_option('display.max_rows', features.shape[0] + 1)
+        #print(features)
 
     def classify(self):
         X = self.feature_block.iloc[:,1:len(self.feature_block.columns)-1]
         y = self.feature_block.iloc[:, -1]
-
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 13)
+        stratifier = [i[-7:] for i in self.feature_block.index]
+        print(set(stratifier))
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, stratify = stratifier)
+        Xtest_stratifier = [i[-7:] for i in X_test.index]
+        Xtrain_stratifier = [i[-7:] for i in X_train.index]
+        print(Counter(Xtest_stratifier))
+        print(Counter(Xtrain_stratifier))
         X_train = scale(X_train)
         X_test = scale(X_test)
-        fit = self.model.fit(X_train, y_train)
+        self.fit = self.model.fit(X_train, y_train)
+        #regularized Log. Regression
+        #Bayesian Logistic Regression
+        #one-vs-all / one-vs-one works
+        #error correcting output codes
+        # multi-class Perceptron / SVM
+        # nearest neighbor, generative probabilistic models
 
-        return self.validateClassifier(fit, X_test, y_test)
+        return self.validateClassifier(self.fit, X_test, y_test)
 
+    def validateClassifier(self, fit, X_test, y_test):
+        y_out = fit.predict(X_test)
+        out_df = pd.DataFrame({'real_y': y_test, 'predicted_y': y_out})
+        plot_confusion_matrix(fit, X_test, y_test, display_labels = ['eth', 'glu', 'nea', 'sal']) #, normalize='true'
+        acc_score = self.accuracy(out_df)
+        print(out_df)
+        print("The accuracy score is: ", acc_score)
+        plt.show()
+        # self.getvalidationset()
 
     def accuracy(self, out_df):
         y_pred = out_df["predicted_y"]
         y_true = out_df["real_y"]
-        score = accuracy_score(y_true, y_pred)
+        score = balanced_accuracy_score(y_true, y_pred)
         return score
 
-    def getvalidationset(self):
+        data = pd.read_csv(mouse_data_file_path)
+        print(data)
+
+'''    def getvalidationset(self):
         mice_data_dir = r'/Users/lucadisse/ETH/Master/FS20/StatsLab/CSV data files for analysis'
         file_regex = r'167-(glu|eth|sal)-IG-[0-9]+_(Brain_signal|Running).csv'
 
         for file in os.listdir(mice_data_dir):
             mouse_data_file_path = os.path.join(mice_data_dir, file)
-            print(mouse_data_file_path)
-
-        data = pd.read_csv(mouse_data_file_path)
-        print(data)
+            print(mouse_data_file_path)'''
--- feature_generator/FeatureExtraction.py
+++ feature_generator/FeatureExtraction.py
@@ -13,68 +13,67 @@ class FeatureExtractor:
     y = pd.Series()
     collected_data = pd.DataFrame()
 
-    def __init__(self, feature_dict, md ,signal_type, mouse_ids={165, 166}, slice_min=30, target="treatment", part_last=10):
+    def __init__(self, feature_dict, md ,signal_type, brain_half = 'left', mouse_ids={165, 166}, slice_min=30,
+                 target="treatment", part_last=10, equal_length = True):
         self.fc_parameters = feature_dict
         self.mouse_data = md
         self.signal_type = signal_type
         self.mouse_ids = mouse_ids
         self.chunk_duration = part_last
-        self.data_preparation(signal_type, slice_min, target, part_last)
-
-
-
-    def getfewFeatures(self, mouse_id=165, signal_type='running', signal_treat='eth', slice_min=0):
-        signal = self.mouse_data.fetch_mouse_signal(mouse_id, signal_treat, signal_type)
-        signal = signal.sliced_data(slice_min=slice_min)
-        signal = signal.get_pandas(time=True)
-
-        id = np.repeat(1, len(signal))
-        id[signal.iloc[:, 0] < 30] = 0
-
-        #fc_parameters = MinimalFCParameters()
-
-        signal.insert(0, "id", id, True)
-        signal.columns = ["id", "time", signal_type]
-
-        extracted_features = extract_features(signal, column_id='id', column_sort='time', column_value=signal_type, default_fc_parameters=self.fc_parameters)
-
-        return extracted_features
-
-    def getFeatures(self, brain_half = 'left'):
-        if brain_half == 'right':
+        self.data_preparation(signal_type, mouse_ids, slice_min, target, part_last, equal_length)
+        if signal_type == 'brain_signal' and brain_half == 'right':
             column_value = self.mouse_data.col_names[self.signal_type][2]
-        elif brain_half == 'both':
+        elif signal_type == 'brain_signal' and brain_half == 'both':
             column_value = self.mouse_data.col_names[self.signal_type][1:3]
         else:
             column_value = self.mouse_data.col_names[self.signal_type][1]
+        self.column_value = column_value
+
+
+    def getFeatures(self, target_class = False):
         extracted_features = extract_features(self.collected_data, column_id='id', column_sort='time_min',
-                         column_value= column_value,
+                         column_value= self.column_value,
                          default_fc_parameters= self.fc_parameters)
+        extracted_features.selection_type = 'all'
+        if target_class:
+            extracted_features['target_class'] = self.y
         return extracted_features
 
     def relevantFeatures(self):
-        features_filtered_direct = extract_relevant_features(self.collected_data, y = self.y, column_id='id', column_sort='time_min', default_fc_parameters=self.fc_parameters)
+        features_filtered_direct = extract_relevant_features(self.collected_data, y = self.y, column_id='id', column_sort='time_min',
+                                                             column_value= self.column_value, default_fc_parameters=self.fc_parameters)
         features_filtered_direct["target_class"] = self.y
+        features_filtered_direct.selection_type = 'relevant'
         return features_filtered_direct
 
 
-    def data_preparation(self, signal_type='running', slice_min=30, target="treatment", part_last=10):
-        mouse_ids = self.mouse_ids
+    def data_preparation(self, signal_type, mouse_ids, slice_min, target, part_last, equal_length):
         mouse_map = list(iter.product(mouse_ids, self.mouse_data.treatments))
         target_map = []
         target_y = []
+        chuncks = []
+        length_subtractor = 0
 
         for j in mouse_map:
             data_gen = self.mouse_data.fetch_mouse_signal(j[0], j[1], signal_type)
             data = data_gen.sliced_data(slice_min=slice_min)
+            previous_chunk_length = len(chuncks)
             chuncks = data.partition_data(part_last=part_last)
+            if equal_length:
+                if j[1] == 'nea':
+                    length = previous_chunk_length - length_subtractor
+                else:
+                    length = len(chuncks)
+            else:
+                length = len(chuncks)+1
 
             chunck_itterator = 0
-            for chunck in chuncks:
+            for chunck in chuncks[0:length-1]:
                 chunck = chunck.get_pandas(time=True)
 
                 # sometimes chuncks have length 0 and we need to skip those chuncks
                 if not len(chunck):
+                    length_subtractor = 1
                     continue
 
                 chunck_itterator = chunck_itterator + 1
