301e78be76b2a0ae7acb26be32a4f7afc2e4307a
mhhmgithub <50996988+mhhmgithub@users.noreply.github.com>
2020-04-27 17:29:32 +0200

    First version of ts reg of running onto brain activity

---
+++ regression/modelling.py
@@ -0,0 +1,219 @@
+'''
+import os as os
+os.chdir('stats_lab')
+sys.path.insert(0, os.getcwd())
+'''
+
+import data_scheduler.lib_data_merger as mice_data
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import cross_val_score
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+#First we want to fetch the relevant data which includes the brain and running activity
+mice_data_dir = r'C:\Users\Massimo\Documents\StatsLab'
+md = mice_data.MiceDataMerger(mice_data_dir)
+treatments = ['glu', 'eth', 'sal', 'nea']
+signals = ['brain_signal', 'running', 'v_o2', 'v_co2', 'rq', 'heat']
+
+#Envelope function
+def envelope(df, window_size=500):
+
+    # We need to smoothen the time series for easier feature extraction
+    # (the larger the window the smoother it will be)
+    windows = df.rolling(window=window_size)
+    df_smooth = windows.mean()
+
+    # plt.plot(brain_smooth.iloc[:,1], )
+    # plt.plot(brain.iloc[:,1])
+    # plt.show()
+
+    # Rectification (all time points are positive)
+    df_rectified = df_smooth.apply(np.abs)
+    df_envelope = df_rectified.rolling(50).mean()
+    df_envelope = df_envelope.dropna()
+    return df_envelope
+
+model = [0,0,0,0]
+residuals = [0,0,0,0]
+
+for i in range(len(treatments)):
+    brain = md.fetch_mouse_signal(mouse_id = 166, treat=treatments[i], signal='brain_signal').get_pandas()
+    running = md.fetch_mouse_signal(mouse_id = 166, treat=treatments[i], signal='running').get_pandas()
+    heat = md.fetch_mouse_signal(mouse_id = 166, treat=treatments[i], signal='heat').get_pandas()
+
+    brain = brain.set_index('time_min')
+    running = running.set_index('time_min')
+    heat = heat.set_index('time_min')
+
+    #We get the envelopes
+    brain_envelope = envelope(brain)
+    running_envelope = envelope(running)
+    heat_envelope = envelope(heat)
+
+    # Now we fit a linear regression model and calculate it's cv score
+    X = pd.concat([brain_envelope.iloc[:, 1], brain_envelope.iloc[:, 1]**2,
+    brain_envelope.iloc[:, 1]**3], axis=1)
+    y = running_envelope
+
+    #Before we fit the model, we standardize the values within the range [0,1]
+
+    mm = MinMaxScaler()
+    mm.fit(X)
+    X = mm.transform(X)
+    X = pd.DataFrame(X).set_index(running_envelope.index)
+
+    mm = MinMaxScaler()
+    mm.fit(y)
+    y = mm.transform(y)
+    y = pd.DataFrame(y).set_index(running_envelope.index)
+
+    #We fit the model with the Ridge linear regression model
+    ridge = Ridge()
+    model[i] = ridge.fit(X,y)
+    prediction = pd.DataFrame(ridge.predict(X)).set_index(running_envelope.index)
+    #We look at the scores
+    scores = cross_val_score(Ridge(), X, y, cv=5)
+    print(scores); print(treatments[i])
+
+    #We look at the residuals
+    residuals[i] = prediction - y
+
+    #We plot the prediction vs. the actual values
+    plt.figure()
+    plt.plot(prediction, label = 'Prediction')
+    plt.plot(y, label = 'Running Activity of 165'+' '+treatments[i])
+    #plt.plot(X.set_index(running_envelope.index), label = 'Brain Activity of 165'+' '+i)
+    plt.title('Time Series Regression: Treatment'+' '+ treatments[i])
+    plt.legend()
+    plt.show()
+
+
+#Look at the distribution of the data
+
+for i in range(len(treatments)):
+    brain = md.fetch_mouse_signal(mouse_id = 166, treat=treatments[i], signal='brain_signal').get_pandas()
+    running = md.fetch_mouse_signal(mouse_id = 166, treat=treatments[i], signal='running').get_pandas()
+    heat = md.fetch_mouse_signal(mouse_id = 166, treat=treatments[i], signal='heat').get_pandas()
+
+    brain = brain.set_index('time_min')
+    running = running.set_index('time_min')
+    plt.figure()
+    plt.hist(brain.iloc[:,1], bins = 100)
+    plt.show()
+
+
+'''
+All about ts regression:
+
+Plotting time series data
+
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots(figsize=(12,6))
+data.plot('date','close',ax=ax)
+ax.set(title='title')
+
+Time series analysis for auditory data: 
+import librosa as lr
+
+audio, sfreq = lr.load('file')
+
+Classification of time series
+
+Smoothing time series
+
+window_size = 50 (the larger the window the smoother it will be)
+windows = audio.rolling(window=window_size)
+aduio_smooth = windowed.mean()
+
+Rectification (all time points are positive)
+audio-rectified = audio.apply(np.abs)
+audio_envelope = audio_rectified.rolling(50).mean()
+
+After we have the envelope of the time series we can do the 
+real feature engineering
+
+envelope_mean = np.mean(audio_envelope, axis = 0)
+...
+envelope_max = np.max(aduio_envelope, axis = 0)
+
+#Now we create our training data for a classifier
+X = np.column_stack([envelope_mean, ..., envelope_max])
+
+y = labels.reshape([-1,1])
+
+Predicting data over time
+
+Interpolation in Pandas
+
+#Return boolean mask
+missing = pices.isna()
+
+#Interpolate linearly within missing windows
+prices_interp = prices.interpolate('linear')
+
+# Create a function we'll use to interpolate and plot
+def interpolate_and_plot(prices, interpolation):
+
+    # Create a boolean mask for missing values
+    missing_values = prices.isna()
+
+    # Interpolate the missing values
+    prices_interp = prices.interpolate(interpolation)
+
+    # Plot the results, highlighting the interpolated values in black
+    fig, ax = plt.subplots(figsize=(10, 5))
+    prices_interp.plot(color='k', alpha=.6, ax=ax, legend=False)
+    
+    # Now plot the interpolated values on top in red
+    prices_interp[missing_values].plot(ax=ax, color='r', lw=3, legend=False)
+    plt.show()
+    
+    
+    # Your custom function
+def percent_change(series):
+    # Collect all *but* the last value of this window, then the final value
+    previous_values = series[:-1]
+    last_value = series[-1]
+
+    # Calculate the % difference between the last value and the mean of earlier values
+    percent_change = (last_value - np.mean(previous_values)) / np.mean(previous_values)
+    return percent_change
+
+# Apply your custom function and plot
+prices_perc = prices.rolling(20).apply(percent_change)
+prices_perc.loc["2014":"2015"].plot()
+plt.show()
+
+
+#Next we want to transform the data to standardizie the variance to detect and remove outliers 
+#After transformation, each point will represent the % change over a previous window
+def percent_change(series):
+    # Collect all *but* the last value of this window, then the final value
+    previous_values = series[:-1]
+    last_value = series[-1]
+
+    # Calculate the % difference between the last value and the mean of earlier values
+    percent_change = (last_value - np.mean(previous_values)) / np.mean(previous_values)
+    return percent_change
+
+# Apply your custom function and plot
+brain_perc = brain.rolling(window=window_size).apply(percent_change)
+
+
+plt.show()
+
+
+
+
+
+Cross validation for fitting time series
+
+    
+    
+
+
+
+'''
