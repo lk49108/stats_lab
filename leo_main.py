3e86000c42fe7cf2a1e8ef60d45da525604e0899
lk49108 <leonardo.21081996@gmail.com>
2020-04-27 15:47:04 +0200

    implemented overlapping chunking, resolved issue with measurements having same (mouse_id, treatment, signal) combination, added two functions for slicing DataFrame to the left and to the right

--- leo_main.py
+++ leo_main.py
@@ -1,7 +1,7 @@
 import data_scheduler.lib_data_merger as data_merger
 
 if __name__ == '__main__':
-    mice_data_dir = r'C:\Users\lkokot\Desktop\ETHZ_STAT_MSC\sem_2\stats_lab\CSV_data_files_for_analysis\CSV data files for analysis'
+    mice_data_dir = r'C:\Users\lkokot\Desktop\ETHZ_STAT_MSC\sem_2\stats_lab\analysis\CSV data files for analysis'
     md = data_merger.MiceDataMerger(mice_data_dir)
     eth = md.fetch_mouse_data(167, 'eth')
     # eth = md.fetch_mouse_signal(167, 'eth', 'rq')
@@ -14,6 +14,6 @@ if __name__ == '__main__':
     print(list(mice_data))
     print(mice_data)
 
-    mice_data = md.fetch_mouse_data([2934, 1650])
+    mice_data = md.fetch_mouse_data([306], signals = 'brain_signal')
     print(list(mice_data))
     print(mice_data)
