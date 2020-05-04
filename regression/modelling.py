cd516146fa085787725a0b17ad91252b1a9389cf
mhhmgithub <50996988+mhhmgithub@users.noreply.github.com>
2020-05-04 11:32:14 +0200

    new version

--- regression/modelling.py
+++ regression/modelling.py
@@ -36,89 +36,115 @@ def envelope(df, window_size=500):
     df_envelope = df_envelope.dropna()
     return df_envelope
 
+# We import all the files from the folder
+name_list = os.listdir(r"C:\Users\Massimo\Documents\StatsLab\New Data")
+print(type(name_list))
+print(name_list)
+
+new_list = []
+
+for i in name_list:
+    new_list.append(i[0:3])
+
+new_set = set(new_list)
+
 model = [0,0,0,0]
 equal, unequal = np.zeros(4), np.zeros(4)
 residuals = [[0],[0],[0],[0]]
-mouse_id = 166
+mouse_ids = [165,166,126, 168, 170, 299, 302, 303, 306, 307, 323, 327]
 objects = ('glu', 'eth', 'sal', 'nea')
 lag_size = 1 #for % changes of time series comparison
 
+for j in range(len(mouse_ids)):
+    for i in range(len(treatments)):
+        brain = md.fetch_mouse_signal(mouse_id = mouse_ids[j], treat=treatments[i], signal='brain_signal').sliced_data(30).get_pandas()
+        running = md.fetch_mouse_signal(mouse_id = mouse_ids[j], treat=treatments[i], signal='running').sliced_data(30).get_pandas()
+        heat = md.fetch_mouse_signal(mouse_id = mouse_ids[j], treat=treatments[i], signal='heat').sliced_data(30).get_pandas()
 
-for i in range(len(treatments)):
-    brain = md.fetch_mouse_signal(mouse_id = mouse_id, treat=treatments[i], signal='brain_signal').get_pandas()
-    running = md.fetch_mouse_signal(mouse_id = mouse_id, treat=treatments[i], signal='running').get_pandas()
-    heat = md.fetch_mouse_signal(mouse_id = mouse_id, treat=treatments[i], signal='heat').get_pandas()
+        brain = brain.set_index('time_min')
+        running = running.set_index('time_min')
+        heat = heat.set_index('time_min')
 
-    brain = brain.set_index('time_min')
-    running = running.set_index('time_min')
-    heat = heat.set_index('time_min')
+        #We get the envelopes
+        brain_envelope = envelope(brain)
+        running_envelope = envelope(running)
+        heat_envelope = envelope(heat)
 
-    #We get the envelopes
-    brain_envelope = envelope(brain)
-    running_envelope = envelope(running)
-    heat_envelope = envelope(heat)
+        print('I got mouse id'+str(mouse_ids[i]))
 
-    # Now we fit a linear regression model and calculate it's cv score
-    X = pd.concat([brain_envelope.iloc[:, 1], brain_envelope.iloc[:, 1]**2,
-    brain_envelope.iloc[:, 1]**3], axis=1)
-    y = running_envelope
+        '''
 
-    #Before we fit the model, we standardize the values within the range [0,1]
+        # Now we fit a linear regression model and calculate it's cv score
+        X = pd.concat([brain_envelope.iloc[:, 1], brain_envelope.iloc[:, 1]**2,
+        brain_envelope.iloc[:, 1]**3], axis=1)
+        y = running_envelope
 
-    mm = MinMaxScaler()
-    mm.fit(X)
-    X = mm.transform(X)
-    X = pd.DataFrame(X).set_index(running_envelope.index)
+        #Before we fit the model, we standardize the values within the range [0,1]
 
-    mm = MinMaxScaler()
-    mm.fit(y)
-    y = mm.transform(y)
-    y = pd.DataFrame(y).set_index(running_envelope.index)
+        mm = MinMaxScaler()
+        mm.fit(X)
+        X = mm.transform(X)
+        X = pd.DataFrame(X).set_index(running_envelope.index)
 
-    #We fit the model with the Ridge linear regression model
-    ridge = Ridge()
-    model[i] = ridge.fit(X,y)
-    prediction = pd.DataFrame(ridge.predict(X)).set_index(running_envelope.index)
-    #We look at the scores
-    scores = cross_val_score(Ridge(), X, y, cv=5)
-    print(scores); print(treatments[i])
+        mm = MinMaxScaler()
+        mm.fit(y)
+        y = mm.transform(y)
+        y = pd.DataFrame(y).set_index(running_envelope.index)
 
-    #We look at the residuals
-    residuals[i] = prediction - y
+        #We fit the model with the Ridge linear regression model
+        ridge = Ridge()
+        model[i] = ridge.fit(X,y)
+        prediction = pd.DataFrame(ridge.predict(X)).set_index(running_envelope.index)
+        #We look at the scores
+        scores = cross_val_score(Ridge(), X, y, cv=5)
+        print(scores); print(treatments[i])
 
-    #We plot the prediction vs. the actual values
-    plt.figure()
-    plt.plot(prediction, label = 'Prediction')
-    plt.plot(y, label = 'Running Activity of 165'+' '+treatments[i])
-    #plt.plot(X.set_index(running_envelope.index), label = 'Brain Activity of 165'+' '+i)
-    plt.title('Time Series Regression: Treatment'+' '+ treatments[i])
-    plt.legend()
-    plt.show()
+        #We look at the residuals
+        residuals[i] = prediction - y
 
-
-    #We output a plot to see if all changes in % of both time series have the same sign
-    equal_sign = ((prediction.pct_change(periods=lag_size)) * (y.pct_change(periods=lag_size))).dropna()
-    equal[i] = ((equal_sign >= 0).sum()/len(equal_sign))
-    unequal[i] = ((equal_sign < 0).sum()/len(equal_sign))
-
-    if i+1 == len(treatments):
+        #We plot the prediction vs. the actual values
         plt.figure()
-        y_pos = np.arange(len(treatments))
-        plt.bar(y_pos, equal, align= 'center')
-        plt.xticks(y_pos, objects)
-        plt.title('Equality of sign for % change')
-        plt.ylim(top=1)
+        plt.plot(prediction, label = 'Prediction')
+        plt.plot(y, label = 'Running Activity of'+' '+str(mouse_ids[j])+' '+treatments[i])
+        #plt.plot(X.set_index(running_envelope.index), label = 'Brain Activity of 165'+' '+i)
+        plt.title('Time Series Regression: Treatment'+' '+ treatments[i]+' '+'of'+' '+str(mouse_ids[j]))
+        plt.legend()
+        plt.show()
 
-        #plt.figure()
-        #plt.bar(y_pos, unequal, align= 'center')
-        #plt.xticks(y_pos, objects)
 
-        # We look at the distribution of the residuals
-        plt.figure()
-        for i in range(len(residuals)):
-            plt.hist(residuals[i].iloc[:, 0], bins=500, label='Residuals of TS Regression' + ' ' + treatments[i])
-            plt.legend()
-            plt.show()
+        #We output a plot to see if all changes in % of both time series have the same sign
+        equal_sign = ((prediction.pct_change(periods=lag_size)) * (y.pct_change(periods=lag_size))).dropna()
+        equal[i] = ((equal_sign >= 0).sum()/len(equal_sign))
+        unequal[i] = ((equal_sign < 0).sum()/len(equal_sign))
+
+        if i+1 == len(treatments):
+            plt.figure()
+            y_pos = np.arange(len(treatments))
+            plt.bar(y_pos, equal, align= 'center')
+            plt.xticks(y_pos, objects)
+            plt.title('Equality of sign for % change of '+str(mouse_ids[j]))
+            plt.ylim(top=1)
+
+            #plt.figure()
+            #plt.bar(y_pos, unequal, align= 'center')
+            #plt.xticks(y_pos, objects)
+
+            # We look at the distribution of the residuals
+            plt.figure()
+            for i in range(len(residuals)):
+
+                plt.hist(residuals[i].iloc[:, 0], bins=500, label='Residuals of TS Regression' + ' ' + treatments[i])
+                plt.legend()
+                plt.title('Residuals of TS Regression of '+str(mouse_ids[j]))
+                plt.show()
+
+#Next we want to collect the regression coefficients for all mice and then contrast them against
+#the saline treatment to see if they are significantly different
+#For this we need to first load the full data of all mice, extract the coefficients and then compare them
+#e.g. via a two-sample t-test
+
+
+
 
 '''
 #Now we implement a function that takes a series as an input and outputs the % change with a given lag level
