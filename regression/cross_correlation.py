c2fcf4e38fc8514ce18a51441420f20ebc801560
mhhmgithub <50996988+mhhmgithub@users.noreply.github.com>
2020-05-18 14:37:37 +0200

    Cross-Correlation now with Spearman corr coefficient and time series regression with shifting of neural activity into positive reals

--- regression/cross_correlation.py
+++ regression/cross_correlation.py
@@ -6,8 +6,8 @@ sys.path.insert(0, os.getcwd())
 
 import data_scheduler.lib_data_merger as mice_data
 import pandas as pd
-import matplotlib.pyplot as plt
-import os as os
+from numpy.lib.stride_tricks import as_strided
+from numpy.lib import pad
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -23,10 +23,11 @@ signals = ['brain_signal', 'running', 'v_o2', 'v_co2', 'rq', 'heat']
 df_anova_r = pd.DataFrame(columns = ['R','Treatment'])
 df_anova_off = pd.DataFrame(columns = ['Offset','Treatment'])
 
-mouse_ids_list = [165, 166, 167, 126, 170, 299, 302, 303, 306, 307, 323, 327]
-mouse_ids = mouse_ids_list[0:12] #(to select all mice use [0:12])
+mouse_ids_list = [165, 166, 167, 168, 126, 170, 299, 302, 303, 306, 307, 323, 327]
+mouse_ids = mouse_ids_list[0:1] #(to select all mice use [0:12])
+window = 200 #for moving correlation plot
 
-def crosscorr(datax, datay, lag=0, wrap=False):
+def crosscorr(datax, datay, method, lag=0, wrap=False):
     """ Lag-N cross correlation.
     Shifted data filled with NaNs
 
@@ -41,40 +42,41 @@ def crosscorr(datax, datay, lag=0, wrap=False):
     if wrap:
         shiftedy = datay.shift(lag)
         shiftedy.iloc[:lag] = datay.iloc[-lag:].values
-        return datax.corr(shiftedy)
+        return datax.corr(shiftedy, method = method)
     else:
-        return datax.corr(datay.shift(lag))
-def plot_crosscorr(brain_signal, running_signal, min_lowerbound, min_upperbound,):
+        return datax.corr(datay.shift(lag), method = method)
+def plot_crosscorr(brain_signal, running_signal, min_lowerbound, min_upperbound, method):
     brain = brain_signal.sliced_data(min_lowerbound).left_slice(min_upperbound).get_pandas()
     brain = brain.set_index('time_min')
     running = running_signal.sliced_data(min_lowerbound).left_slice(min_upperbound).get_pandas()
     running = running.set_index('time_min')
 
-    d1 = brain.iloc[:, 1]
+    d1 = brain.iloc[:, 0]
     d2 = running.iloc[:, 0]
-    minutes = 30
-    rs = [crosscorr(d1, d2, lag) for lag in range(-int(minutes), int(minutes))]
+    shift = 30
+    rs = [crosscorr(d1, d2, method, lag) for lag in range(-int(shift), int(shift))]
     offset = np.ceil(len(rs) / 2) - np.argmax(rs)
     f, ax = plt.subplots(figsize=(14, 3))
     ax.plot(rs)
     ax.axvline(np.ceil(len(rs) / 2), color='k', linestyle='--', label='Center')
     ax.axvline(np.argmax(rs), color='r', linestyle='--', label='Peak synchrony')
-    ax.set(title=f'Offset = {offset} minutes\n Brain signal leads <> Running signal leads \n + str(mouse_ids[j]) + str(treatments[i])',
+    ax.set(title=f'Offset = {offset} shift\n Brain signal leads <> Running signal leads \n' + str(mouse_ids[j]) + str(treatments[i]),
            xlabel='Offset',
-           ylabel='Pearson r')
+           ylabel= corr_method + ' correlation coefficient')
+
     plt.legend()
-def return_crosscorr(brain_signal, running_signal, min_lowerbound, min_upperbound):
+def return_crosscorr(brain_signal, running_signal, min_lowerbound, min_upperbound, method):
     brain = brain_signal.sliced_data(min_lowerbound).left_slice(min_upperbound).get_pandas()
     brain = brain.set_index('time_min')
     running = running_signal.sliced_data(min_lowerbound).left_slice(min_upperbound).get_pandas()
     running = running.set_index('time_min')
 
-    d1 = brain.iloc[:, 1]
+    d1 = brain.iloc[:, 0]
     d2 = running.iloc[:, 0]
-    minutes = 30
-    rs = [crosscorr(d1, d2, lag) for lag in range(-int(minutes), int(minutes))]
+    shift = 30
+    rs = [crosscorr(d1, d2, method, lag) for lag in range(-int(shift), int(shift))]
     return np.max(rs)
-def return_offset(brain_signal, running_signal, min_lowerbound, min_upperbound):
+def return_offset(brain_signal, running_signal, min_lowerbound, min_upperbound, method):
     brain = brain_signal.sliced_data(min_lowerbound).left_slice(min_upperbound).get_pandas()
     brain = brain.set_index('time_min')
     running = running_signal.sliced_data(min_lowerbound).left_slice(min_upperbound).get_pandas()
@@ -83,50 +85,102 @@ def return_offset(brain_signal, running_signal, min_lowerbound, min_upperbound):
     d1 = brain.iloc[:, 1]
     d2 = running.iloc[:, 0]
     minutes = 30
-    rs = [crosscorr(d1, d2, lag) for lag in range(-int(minutes), int(minutes))]
+    rs = [crosscorr(d1, d2, method, lag) for lag in range(-int(minutes), int(minutes))]
     offset = np.ceil(len(rs) / 2) - np.argmax(rs)
     return offset
+def rolling_correlation_plot(brain_signal, running_signal,  min_lowerbound, min_upperbound, window_size = window):
+    brain = brain_signal.sliced_data(min_lowerbound).left_slice(min_upperbound).get_pandas()
+    brain = brain.set_index('time_min')
+    running = running_signal.sliced_data(min_lowerbound).left_slice(min_upperbound).get_pandas()
+    running = running.set_index('time_min')
+
+    d1 = brain.iloc[:, 0]
+    d2 = running.iloc[:, 0]
+
+
+    # Compute rolling window synchrony
+    rolling_r = pd.Series(rolling_spearman(d1, d2, window_size))
+    f,ax=plt.subplots(2,1,figsize=(14,6),sharex=True)
+    d1.rolling(window=30,center=True).median().plot(ax=ax[0])
+    d2.rolling(window=30,center=True).median().plot(ax=ax[0])
+    ax[0].set(xlabel='Minutes',ylabel='Brain/running signal (Rolling median)')
+    rolling_r.plot(ax=ax[1])
+    ax[1].set(xlabel='Minutes',ylabel='Spearman correlation coefficient')
+    plt.suptitle('Brain/running signal and rolling window correlation for ' + str(mouse_ids[j]) + str(treatments[i]))
+
+def rolling_spearman(seqa, seqb, window):
+    stridea = seqa.strides[0]
+    ssa = as_strided(seqa, shape=[len(seqa) - window + 1, window], strides=[stridea, stridea])
+    strideb = seqa.strides[0]
+    ssb = as_strided(seqb, shape=[len(seqb) - window + 1, window], strides =[strideb, strideb])
+    ar = pd.DataFrame(ssa)
+    br = pd.DataFrame(ssb)
+    ar = ar.rank(1)
+    br = br.rank(1)
+    corrs = ar.corrwith(br, 1)
+    output = pd.Series(pad(corrs, (window - 1, 0), 'constant', constant_values=np.nan))
+    output.index = seqa.index
+    return output
 
 #Interval in minutes
 lower = 30
-upper = 70
+upper = 90
+
+#Parameters
+option_plot_crosscorr = False
+corr_method = 'spearman' #pearson (for linear relationship); spearman for monotone relationship (direction matters)
+option_moving_correlation = True
+
 
 #We loop through all mice and treatments to get two columns: Pearson R and the respective treatment
 for j in range(len(mouse_ids)):
     for i in range(len(treatments)):
         brain_signal = md.fetch_mouse_signal(mouse_id = mouse_ids[j], treat=treatments[i], signal='brain_signal')
         running_signal = md.fetch_mouse_signal(mouse_id = mouse_ids[j], treat=treatments[i], signal='running')
-        '''
-        # Visualization of data
-        if brain_signal is not None and running_signal is not None:
-            if type(brain_signal) is not list and type(running_signal) is not list:
-                plot_crosscorr(brain_signal, running_signal)
 
-            elif type(brain_signal) is list and type(running_signal) is list:
-                for m in range(len(brain_signal)):
-                    plot_crosscorr(brain_signal[m], running_signal[m])
-        '''
+        if option_plot_crosscorr:
+            # Visualization of data
+            if brain_signal is not None and running_signal is not None:
+                if type(brain_signal) is not list and type(running_signal) is not list:
+                    plot_crosscorr(brain_signal, running_signal, lower, upper, method=corr_method)
+
+                elif type(brain_signal) is list and type(running_signal) is list:
+                    for m in range(len(brain_signal)):
+                        plot_crosscorr(brain_signal[m], running_signal[m], lower, upper, method=corr_method)
+
 
         # Output of optimal Pearson r value and offset
         if brain_signal is not None and running_signal is not None:
             if type(brain_signal) is not list and type(running_signal) is not list:
-                df_anova_r = df_anova_r.append(pd.DataFrame({'R': [return_crosscorr(brain_signal, running_signal, lower, upper)],
+                df_anova_r = df_anova_r.append(pd.DataFrame({'R': [return_crosscorr(brain_signal, running_signal, lower, upper, method=corr_method)],
                                               'Treatment': [treatments[i]]}))
-                df_anova_off = df_anova_off.append(pd.DataFrame({'Offset': [return_offset(brain_signal, running_signal, lower, upper)],
+                df_anova_off = df_anova_off.append(pd.DataFrame({'Offset': [return_offset(brain_signal, running_signal, lower, upper, method=corr_method)],
                                                              'Treatment': [treatments[i]]}))
 
 
             elif type(brain_signal) is list and type(running_signal) is list:
                 for m in range(len(brain_signal)):
-                    df_anova_r = df_anova_r.append(pd.DataFrame({'R': [return_crosscorr(brain_signal[m], running_signal[m], lower, upper)],
+                    df_anova_r = df_anova_r.append(pd.DataFrame({'R': [return_crosscorr(brain_signal[m], running_signal[m], lower, upper, method=corr_method)],
                                                   'Treatment': [treatments[i]]}))
-                    df_anova_off = df_anova_off.append(pd.DataFrame({'Offset': [return_offset(brain_signal[m], running_signal[m], lower, upper)],
+                    df_anova_off = df_anova_off.append(pd.DataFrame({'Offset': [return_offset(brain_signal[m], running_signal[m], lower, upper, method=corr_method)],
                                       'Treatment': [treatments[i]]}))
 
         else:
             print(mouse_ids[j], treatments[i])
 
+        if option_moving_correlation:
+            # Visualization of data
+            if brain_signal is not None and running_signal is not None:
+                if type(brain_signal) is not list and type(running_signal) is not list:
+                    rolling_correlation_plot(brain_signal, running_signal, lower, upper, window_size = window)
+
+                elif type(brain_signal) is list and type(running_signal) is list:
+                    for m in range(len(brain_signal)):
+                        rolling_correlation_plot(brain_signal[m], running_signal[m], lower, upper, window_size = window)
+
+
 #We perform a simple one-way ANOVA for optimal R values
+print(df_anova_r)
 aov_r = pg.anova(data=df_anova_r, dv='R', between='Treatment', ss_type=2, detailed=True)
 print(aov_r)
 aov_off = pg.anova(data=df_anova_off, dv='Offset', between='Treatment', ss_type=2, detailed=True)
@@ -135,13 +189,10 @@ print(aov_off)
 
 #We visualize the results for boxplots
 df_anova_r.boxplot(column='R', by='Treatment')
-plt.title(label='Boxplot for Pearson R across treatments')
+plt.title(label='Boxplot for ' + corr_method + ' correlation coefficient across treatments')
+plt.ylabel(ylabel= (corr_method + ' correlation coefficient'))
 
 df_anova_off.boxplot(column='Offset', by='Treatment')
 plt.title(label='Boxplot for Offset across treatments')
+plt.ylabel(ylabel='Shift (in time period of time series)')
 plt.show()
-
-
-
-
-
--- regression/modelling.py
+++ regression/modelling.py
@@ -6,16 +6,14 @@ sys.path.insert(0, os.getcwd())
 
 import data_scheduler.lib_data_merger as mice_data
 import matplotlib.pyplot as plt
-import os as os
 import numpy as np
 from sklearn.linear_model import Ridge
-from sklearn.model_selection import cross_val_score
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from statsmodels.graphics.tsaplots import plot_acf
 from statsmodels.graphics.tsaplots import plot_pacf
 from statsmodels.multivariate.manova import MANOVA
-
+import pingouin as pg
 
 
 #First we want to fetch the relevant data which includes the brain and running activity
@@ -24,67 +22,57 @@ md = mice_data.MiceDataMerger(mice_data_dir)
 treatments = ['glu', 'eth', 'sal', 'nea']
 signals = ['brain_signal', 'running', 'v_o2', 'v_co2', 'rq', 'heat']
 
-#Envelope function
-def envelope(df, window_size=500):
+#We want to shift the brain signal into the set of real positive numbers to make
+#the times series regression more coherent
 
-    # We need to smoothen the time series for easier feature extraction
-    # (the larger the window the smoother it will be)
-    windows = df.rolling(window=window_size)
-    df_smooth = windows.mean()
-
-    # plt.plot(brain_smooth.iloc[:,1], )
-    # plt.plot(brain.iloc[:,1])
-    # plt.show()
-
-    # Rectification (all time points are positive)
-    df_rectified = df_smooth.apply(np.abs)
-    df_envelope = df_rectified.rolling(50).mean()
-    df_envelope = df_envelope.dropna()
-    return df_envelope
+def transform(df):
+    df_shifted = df + 3
+    return df_shifted
 
 #Ridge regression with polynomial features d=3
 def get_ridge_coef(brain, running, plot_prediction = False, include_brain_signal = False):
-    # We get the envelopes
-    brain_envelope = envelope(brain)
-    running_envelope = envelope(running)
+    # We get the shifts
+    brain_shift = transform(brain)
+    running_shift = transform(running)
 
-    X = pd.concat([brain_envelope.iloc[:, 1], brain_envelope.iloc[:, 1] ** 2,
-                   brain_envelope.iloc[:, 1] ** 3], axis=1)
-    y = running_envelope
+    X = brain_shift.iloc[:, 1].values.reshape(-1, 1)
+    y = running_shift
 
     # Before we fit the model, we standardize the values within the range [0,1]
     mm = MinMaxScaler()
 
     mm.fit(X)
     X = mm.transform(X)
-    X = pd.DataFrame(X).set_index(running_envelope.index)
+    X = pd.DataFrame(X).set_index(running_shift.index)
     mm = MinMaxScaler()
 
     mm.fit(y)
     y = mm.transform(y)
-    y = pd.DataFrame(y).set_index(running_envelope.index)
+    y = pd.DataFrame(y).set_index(running_shift.index)
 
     # We fit the model with the Ridge linear regression model
     ridge = Ridge(normalize=True, fit_intercept=False)
     ridge.fit(X, y)
-    prediction = pd.DataFrame(ridge.predict(X)).set_index(running_envelope.index)
+    prediction = pd.DataFrame(ridge.predict(X)).set_index(running_shift.index)
 
     if plot_prediction:
         # Transform brain signal to plot it
         mm = MinMaxScaler()
-        mm.fit(np.array(brain_envelope.iloc[:, 1]).reshape(-1,1))
-        brain_envelope_transformed = mm.transform(np.array(brain_envelope.iloc[:, 1]).reshape(-1,1))
-        brain_envelope_transformed = pd.DataFrame(brain_envelope_transformed).set_index(running_envelope.index)
+        mm.fit(np.array(brain_shift.iloc[:, 1]).reshape(-1,1))
+        brain_shift_transformed = mm.transform(np.array(brain_shift.iloc[:, 1]).reshape(-1,1))
+        brain_shift_transformed = pd.DataFrame(brain_shift_transformed).set_index(running_shift.index)
 
         # We plot the prediction of the running signal, the actual running signal and the regressor (brain signal)
         plt.figure()
         plt.plot(prediction, label='Prediction')
         if include_brain_signal:
-            plt.plot(brain_envelope_transformed, label='Brain signal (envelope)')
-        plt.plot(y, label='Running signal (envelope) of' + ' ' + str(mouse_ids[j]) + ' ' + treatments[i])
-        plt.title('Time Series Regression: Treatment' + ' ' + treatments[i] + ' ' + 'of' + ' ' + str(mouse_ids[j]))
-        plt.legend()
-        plt.show()
+            plt.plot(brain_shift_transformed, label='Brain signal (shift)')
+            plt.plot(y, label='Running signal (shift) of' + ' ' + str(mouse_ids[j]) + ' ' + treatments[i])
+            plt.title('Time Series Regression: Treatment' + ' ' + treatments[i] + ' ' + 'of' + ' ' + str(mouse_ids[j]))
+            plt.xlabel(xlabel='Minutes')
+            plt.ylabel(ylabel='Signal value')
+            plt.legend()
+            plt.show()
 
         # We look at the residuals
         # residuals[i] = prediction - y
@@ -99,9 +87,9 @@ def preprocessing(signal, min_lowerbound, min_upperbound):
 
 #Defining global variables
 mouse_ids_list = [165, 166, 167, 126, 170, 299, 302, 303, 306, 307, 323, 327]
-mouse_ids = mouse_ids_list[9:12] #(to select all mice use [0:12])
+mouse_ids = mouse_ids_list[0:12] #(to select all mice use [0:12])
 objects = ('glu', 'eth', 'sal', 'nea')
-df_anova_coef = pd.DataFrame(columns = ['coef1','coef2','coef3','Treatment'])
+df_anova_coef = pd.DataFrame(columns = ['coef1','Treatment'])
 
 #Defining parameters
 option_acf = False #set to True if want to plot acf
@@ -109,7 +97,8 @@ option_pacf = False #set to True if want to plot acf
 lags_correlogram = 100 #set lag size for correlogram
 
 option_scatter = False #set to True if want to plot scatterplot of brain vs. running activity
-option_plot_prediction = True #set to True if want to plot prediction vs. actual signal
+option_plot_prediction = False #set to True if want to plot prediction vs. actual signal
+option_brain_signal = False #set to True if want to plot brain signal as well
 
 #Interval in minutes
 lower = 30
@@ -127,30 +116,68 @@ for j in range(len(mouse_ids)):
                 brain = preprocessing(brain_signal, lower, upper)
                 running = preprocessing(running_signal, lower, upper)
 
-                coefficients = np.abs(get_ridge_coef(brain, running, option_plot_prediction))
+                coefficients = get_ridge_coef(brain, running, option_plot_prediction, include_brain_signal=option_brain_signal)
 
                 df_anova_coef = df_anova_coef.append(pd.DataFrame({ 'coef1': coefficients[0:,0],
-                                                                    'coef2': coefficients[0:,1],
-                                                                    'coef3': coefficients[0:,2],
                                                                     'Treatment': [treatments[i]]}))
                 print(coefficients)
+                if option_acf:
+                    # ACF: We want to discover if there is a specific dependency structure of the time series
+                    plot_acf(brain.iloc[:, 1], lags=lags_correlogram,
+                             title='Autocorrelation Brain Activity' + str(mouse_ids[j]) + str(treatments[i]))
+                    plot_acf(running, lags=lags_correlogram,
+                             title='Autocorrelation Running Activity' + str(mouse_ids[j]) + str(treatments[i]))
+                    plt.show()
+
+                if option_pacf:
+                    # PACF: We want to discover if there is a specific dependency structure of the time series
+                    plot_pacf(brain.iloc[:, 1], lags = lags_correlogram, title='PACF Brain Activity'+str(mouse_ids[j])+str(treatments[i]))
+                    plot_pacf(running, lags = lags_correlogram, title='PACF Running Activity'+str(mouse_ids[j])+str(treatments[i]))
+                    plt.show()
+
+
+
+
+
 
             elif type(brain_signal) is list and type(running_signal) is list:
                 for m in range(len(brain_signal)):
                     brain = preprocessing(brain_signal[m], lower, upper)
                     running = preprocessing(running_signal[m], lower, upper)
 
-                    coefficients = np.abs(get_ridge_coef(brain, running, option_plot_prediction))
+                    coefficients = get_ridge_coef(brain, running, option_plot_prediction, include_brain_signal=option_brain_signal)
                     df_anova_coef = df_anova_coef.append(pd.DataFrame({'coef1': coefficients[0:,0],
-                                                                       'coef2': coefficients[0:,1],
-                                                                       'coef3': coefficients[0:,2],
                                                                        'Treatment': [treatments[i]]}))
+
+                    if option_acf:
+                        # ACF: We want to discover if there is a specific dependency structure of the time series
+                        plot_acf(brain.iloc[:, 1], lags=lags_correlogram,
+                                 title='Autocorrelation Brain Activity' + str(mouse_ids[j]) + str(treatments[i]))
+                        plot_acf(running, lags=lags_correlogram,
+                                 title='Autocorrelation Running Activity' + str(mouse_ids[j]) + str(treatments[i]))
+                        plt.show()
+
+                    if option_pacf:
+                        # PACF: We want to discover if there is a specific dependency structure of the time series
+                        plot_pacf(brain.iloc[:, 1], lags=lags_correlogram,
+                                  title='PACF Brain Activity' + str(mouse_ids[j]) + str(treatments[i]))
+                        plot_pacf(running, lags=lags_correlogram,
+                                  title='PACF Running Activity' + str(mouse_ids[j]) + str(treatments[i]))
+                        plt.show()
         else:
             print(mouse_ids[j], treatments[i])
 
 
 
 #We perform a simple one-way ANOVA for optimal R values
-maov = MANOVA.from_formula('coef1 + coef2 + coef3  ~ Treatment', data=df_anova_coef)
-print(maov.mv_test())
+#maov = MANOVA.from_formula('coef1 + coef2 + coef3  ~ Treatment', data=df_anova_coef)
+#print(maov.mv_test())
 
+df_anova_coef.boxplot(column='coef1', by='Treatment')
+plt.title(label='Boxplot for Ridge regression coefficient across treatments')
+plt.ylabel(ylabel='Ridge regression coefficient')
+plt.show()
+
+#We perform a simple one-way ANOVA for Ridge regresion coefficients values
+aov_coef = pg.anova(data=df_anova_coef, dv='coef1', between='Treatment', ss_type=2, detailed=True)
+print(aov_coef)
